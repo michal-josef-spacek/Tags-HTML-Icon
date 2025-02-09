9a69894ba0cb3eff5937cedd284150515abd7eff
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2025-02-09 14:25:06 +0100

    First version

---
+++ Icon.pm
@@ -0,0 +1,104 @@
+package Tags::HTML::Icon;
+
+use base qw(Tags::HTML);
+use strict;
+use warnings;
+
+use Class::Utils qw(set_params split_params);
+use Error::Pure qw(err);
+use Scalar::Util qw(blessed);
+
+our $VERSION = 0.01;
+
+# Constructor.
+sub new {
+	my ($class, @params) = @_;
+
+	# Create object.
+	my ($object_params_ar, $other_params_ar) = split_params(
+		['css_class'], @params);
+	my $self = $class->SUPER::new(@{$other_params_ar});
+
+	# CSS style for list.
+	$self->{'css_class'} = 'icon';
+
+	# Process params.
+	set_params($self, @{$object_params_ar});
+
+	# Object.
+	return $self;
+}
+
+sub _cleanup {
+	my $self = shift;
+
+	delete $self->{'_icon'};
+
+	return;
+}
+
+sub _init {
+	my ($self, $icon) = @_;
+
+	if (! defined $icon || ! blessed($icon) || ! $icon->isa('Data::Icon')) {
+		err 'Data object for icon is not valid.';
+	}
+
+	$self->{'_icon'} = $icon;
+
+	return;
+}
+
+# Process 'Tags'.
+sub _process {
+	my $self = shift;
+
+	if (! exists $self->{'_icon'}) {
+		return;
+	}
+
+	$self->{'tags'}->put(
+		['b', 'span'],
+		['a', 'class', $self->{'css_class'}],
+	);
+	if (defined $self->{'_icon'}->url) {
+		$self->{'tags'}->put(
+			['b', 'img'],
+			defined $self->{'_icon'}->alt ? (
+				['a', 'alt', $self->{'_icon'}->alt],
+			) : (),
+			['a', 'src', $self->{'_icon'}->url],
+			['e', 'img'],
+		);
+	} else {
+		$self->{'tags'}->put(
+			defined $self->{'_icon'}->color ? (
+				['b', 'span'],
+				['a', 'color', $self->{'_icon'}->color],
+			) : (),
+			['d', $self->{'_icon'}->char],
+			defined $self->{'_icon'}->color ? (
+				['e', 'span'],
+			) : (),
+		);
+	}
+	$self->{'tags'}->put(
+		['e', 'span'],
+	);
+
+	return;
+}
+
+sub _process_css {
+	my $self = shift;
+
+	$self->{'css'}->put(
+		['s', '.'.$self->{'css_class'}],
+		# TODO
+		['e'],
+	);
+
+	return;
+}
+
+1;
